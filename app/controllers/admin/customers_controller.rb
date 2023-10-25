1282b67e266ceaadef51ec87c1b59da8afad1fda
EC2 Default User <ec2-user@ip-172-31-87-180.ec2.internal>
2023-10-25 18:45:15 +0000

    [Add]

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,8 @@
 class Admin::CustomersController < ApplicationController
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_active)
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_admin!, if: :admin_url
-  
-  def admin_url
-    request.fullpath.include?("/admin")
-  end
   
   def after_sign_in_path_for(resource)
     about_psth
