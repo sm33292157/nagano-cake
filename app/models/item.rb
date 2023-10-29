12569cb34a5c9070a1298cc4a9fc5b3986866540
EC2 Default User <ec2-user@ip-172-31-87-180.ec2.internal>
2023-10-29 06:54:27 +0000

    [Add]

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,12 @@ class Item < ApplicationRecord
     validates :image
   end
   
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/naganocake.logo.PNG')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [300, 300]).processed
+    image.variant(resize_to_limit: [width, height]).processed
   end
   
   def add_tax_price
