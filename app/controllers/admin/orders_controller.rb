ee36b6ff68cf873e6f2f88f9f703173b50c1af5a
EC2 Default User <ec2-user@ip-172-31-87-180.ec2.internal>
2023-11-05 15:25:48 +0000

    [Add]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,13 @@
 class Admin::OrdersController < ApplicationController
+  
+  def new
+    @order = Order.new
+    @addresses = current_customer.addresses.all
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:customer_id, :name, :address, :postal_code, :shipping_fee, :payment_method, :payment_amount, :order_status)
+  end
 end
