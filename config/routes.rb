a83cac3e5b62a2caa3afc348fc1ccf9aacb18f2c
EC2 Default User <ec2-user@ip-172-31-87-180.ec2.internal>
2023-11-08 07:43:53 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
